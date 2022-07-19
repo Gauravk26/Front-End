28f0a4572a66f931bcec512f111b8ac0fb10578a
Gauravk26 <gauravk446159@gmail.com>
2022-07-19 22:01:41 +0530

    More Coding Questions

---
+++ Coding/moveZeroes.js
@@ -0,0 +1,18 @@
+// i/p-> [1,0,2,3,0,5,0]
+// o/p-> [1,2,3,5,0,0,0]
+
+var arr = [1,0,2,3,0,5,0]
+function moveZeroes(newArr){
+    let count = 0;
+    for(var i =0;i<newArr.length;i++){
+        if(newArr[i] !== 0){
+          newArr[count++] = newArr[i]
+        }
+    }
+    for(var i = count;i<newArr.length;i++){
+        newArr[i] = 0
+    }
+    return newArr
+}
+
+console.log(moveZeroes(arr))
